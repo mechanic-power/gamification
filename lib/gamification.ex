744907b5a1eff526fdcc7cc39973f80d59f0038e
Sascha Brink <sascha.brink@gmail.com>
2020-05-18 22:43:36 +0200

    Initial commit

---
+++ lib/gamification.ex
@@ -0,0 +1,18 @@
+defmodule Gamification do
+  @moduledoc """
+  Documentation for `Gamification`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gamification.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gamification_test.exs
@@ -0,0 +1,8 @@
+defmodule GamificationTest do
+  use ExUnit.Case
+  doctest Gamification
+
+  test "greets the world" do
+    assert Gamification.hello() == :world
+  end
+end
