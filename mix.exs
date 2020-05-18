6ed13ae7c4a3655ff16b2f607dd48abe74a0e31d
Martin Schurig <martin@schurig.pw>
2020-05-18 22:52:23 +0200

    Add credo, test.watch and Licence

--- mix.exs
+++ mix.exs
@@ -5,9 +5,19 @@ defmodule Gamification.MixProject do
     [
       app: :gamification,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.9",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.html": :test,
+        "coveralls.github": :test,
+        credo: :test,
+        "test.watch": :test
+      ]
     ]
   end
 
@@ -18,11 +28,15 @@ defmodule Gamification.MixProject do
     ]
   end
 
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, ">= 0.0.0", only: :test},
+      {:excoveralls, "~> 0.7", only: :test},
+      {:mix_test_watch, "~> 1.0", only: :test, runtime: false}
     ]
   end
 end
